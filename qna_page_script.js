98f94f26de9b16a7a0da612447efef275e0ebc19
Shubham Singh <36928095+shubhams167@users.noreply.github.com>
2021-01-03 01:32:35 +0530

    Improved html content

--- qna_page_script.js
+++ qna_page_script.js
@@ -1,8 +1,34 @@
 const getAnswer = () => {
 	try {
-		const answer = document.getElementsByClassName("answer-given-body")[0]
-			.innerHTML;
-		return answer;
+		// Get question
+		const question = document.querySelector(".question-body-text").innerHTML;
+
+		// Get number of likes and dislikes
+		let likes = document.querySelector(".up-count")?.innerHTML;
+		if (!likes) likes = "0";
+		let dislikes = document.querySelector(".down-count")?.innerHTML;
+		if (!dislikes) dislikes = "0";
+
+		// Get answer
+		const answer = document.querySelector(".answer-given-body").innerHTML;
+
+		const html = `	<h2><u>QUESTION</u></h2>
+						<div style="border-radius: 1px solid black; border: 2px solid black; padding: 5px;">
+							${question}
+						</div>
+						<br>
+						<h2><u>STATS</u></h2>
+						<div style="border-radius: 1px solid black; border: 2px solid black; padding: 5px;">
+							<h3>Likes: ${likes} Dislikes: ${dislikes}</h3>
+						</div>
+						<br>
+						<h2><u>ANSWER</u></h2>
+						<div style="border-radius: 1px solid black; border: 2px solid black; padding: 5px;">
+							${answer}
+						</div>
+						`;
+
+		return html;
 	} catch (err) {
 		return null;
 	}
